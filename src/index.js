cb31334a78768afa9777ef33d66c2010ef70e887
elmarmos <alejandromarmolejos@hotmail.com>
2022-01-22 14:25:25 -0500

    Google Analytics testing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
-import ReactGA from "react-ga";
 import theme from "./constants/theme";
 import { ThemeProvider } from "styled-components";
 
-ReactGA.initialize("G-Z7TXXF4WYK");
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import "../assets/styles/index.css";
 import Header from "../components/Header";
-
+import ReactGA from "react-ga";
 import data from "../constants/data";
+import { useEffect } from "react";
 
 const HomeContainer = styled.div`
   background-color: ${(props) => props.theme.mainBackground};
@@ -10,6 +11,18 @@ const HomeContainer = styled.div`
 `;
 
 function Home() {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      ReactGA.initialize("G-Z7TXXF4WYK", {
+        debug: true,
+      });
+      window.GA_INITIALIZED = true;
+    }
+
+    ReactGA.pageview("/", null, "Page visit");
+  }, []);
+
+  
   return (
     <HomeContainer>
       <Header data={data.header}></Header>
